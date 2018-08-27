71561672c42ce921f3cca12cf550f7b951bf9ba1
therogerwang <32213225+therogerwang@users.noreply.github.com>
2018-08-27 15:19:28 -0500

    Created Registration, extended bootstrap base

--- coincidence/settings.py
+++ coincidence/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'market.apps.MarketConfig',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- coincidence/urls.py
+++ coincidence/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('market/', include('market.urls'))
 ]
