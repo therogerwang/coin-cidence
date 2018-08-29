999069a9ece331e953da6d9a90a80d17879bc505
therogerwang <32213225+therogerwang@users.noreply.github.com>
2018-08-28 20:31:06 -0500

    Post URL setup

--- market/views.py
+++ market/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import User, Coin
 from .forms import QuantityForm
+
 
 def index(request):
     return render(request, 'market/main.html',)
     
     
 
-
+def transact(request):
+    
+    print ( request.POST['transactionQuantity'])
+    
+    return HttpResponseRedirect(reverse('index'))
     
