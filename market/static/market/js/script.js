639b243b7cf445ac3fa282e17e0211fe1fa263b0
therogerwang <32213225+therogerwang@users.noreply.github.com>
2018-09-05 00:03:38 -0500

    updated POST request

--- market/static/market/js/script.js
+++ market/static/market/js/script.js
@@ -1,8 +1,8 @@
 
 
 
-//COOKIE SYSTEM
-document.cookie = "userdata="+JSON.stringify({money: 1000, baz: 'poo'});
+// //COOKIE SYSTEM
+// document.cookie = "userdata="+JSON.stringify({money: 1000, baz: 'poo'});
 
 
 //PRICE UPDATING BASED ON API
@@ -20,10 +20,9 @@ function send_request(){
         // GET data access
         var data = JSON.parse(this.response);
 
-        console.log(data);
-        console.log("BTC PRICE = ", data.USD);
         document.getElementById("BTC_price").innerHTML = "$ "+data.USD;
-
+        document.getElementById("transact_price").value = data.USD;
+        console.log("transact_price =", document.getElementById("transact_price").value)
     }
 
     request.send();
@@ -41,34 +40,3 @@ setInterval(function(){
 
 
 
-//NANO
-function send_request(){
-
-    // CoinCompare API ACCESS
-    var request = new XMLHttpRequest();
-
-    request.open("GET","https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD", true);
-
-    request.onload= function(){
-
-        // GET data access
-        var data = JSON.parse(this.response);
-
-        console.log(data);
-        console.log("BTC PRICE = ", data.USD);
-        document.getElementById("BTC_price").innerHTML = "$ "+data.USD;
-
-    }
-
-    request.send();
-
-}
-
-send_request();
-
-setInterval(function(){
-
-    send_request();
-    
-//UPDATE EVERY 10 SECONDS
-},10*1000);
--- market/views.py
+++ market/views.py
@@ -3,20 +3,40 @@ from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import User, Coin
 from .forms import QuantityForm
+from decimal import *
+
+
+
+#SETTING DECIMAL BASED MATH
+getcontext().prec = 8
 
 
 def index(request):
     return render(request, 'market/main.html',)
     
-    
-#request arrives, decides what to do
+
 def transact(request):
+    """ Decides what happens with the Transact (buy/sell) POST request when it 
+    is received """
     
-    print ( request.POST['transactionQuantity'])
+
+    transactQuantity = Decimal(request.POST['transactionQuantity'])
+    coin_type = request.POST['coin_type']
+    transact_price = request.POST['transact_price']
+    print (transactQuantity, coin_type, transact_price)
+    
+    
     
     try:
+        #BUY
         print ( request.POST['buy'])
+        
+        
+        
+        
+        
     except:
+        #SELL
         print ( request.POST['sell'])
     
     print("After")
